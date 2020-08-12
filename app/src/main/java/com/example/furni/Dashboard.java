5752a6ec6dd907b5fd1bdf681359c15077b6c465
Nandini Bagga <bagganandini8@gmail.com>
2020-08-13 01:46:44 +0530

    Search view added

--- app/src/main/java/com/example/furni/Dashboard.java
+++ app/src/main/java/com/example/furni/Dashboard.java
@@ -4,15 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Filterable;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.widget.Toolbar;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -23,13 +30,21 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+
+import java.util.ArrayList;
+
 public class Dashboard extends AppCompatActivity {
 
-    ImageView signout;
+    TextView name, email;
+    ImageView signout, profile_photo;
     FirebaseAuth mAuth;
     GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
-
+    SearchView mySearchView;
+    ListView myList;
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
+    private static String [] SUGGESTION = new String[] {"Legs, Hands"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +70,34 @@ public class Dashboard extends AppCompatActivity {
 //            }
 //        };
 
+        name = findViewById(R.id.username);
+        profile_photo = findViewById(R.id.profile_photo);
+        mySearchView = findViewById(R.id.search_bar);
+
+        myList = findViewById(R.id.myList);
+        list = new ArrayList<String>();
+        list.add("Legs");
+        list.add("hands");
+
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,list);
+
+        myList.setAdapter(adapter);
+
+
+        mySearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
         signout = findViewById(R.id.signout_button);
         signout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,19 +112,20 @@ public class Dashboard extends AppCompatActivity {
                 }
             }
         });
-//        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
-//        if (acct != null) {
-//            String personName = acct.getDisplayName();
-//
-//            String personEmail = acct.getEmail();
-//
-//            Uri personPhoto = acct.getPhotoUrl();
-//
-//            name.setText(personName);
-//            email.setText(personEmail);
-//            Glide.with(this).load(String.valueOf(personPhoto)).into(imageView);
-//        }
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+
+            String personEmail = acct.getEmail();
+
+            Uri personPhoto = acct.getPhotoUrl();
+
+            name.setText(personName);
+           // email.setText(personEmail);
+            Glide.with(this).load(String.valueOf(personPhoto)).into(profile_photo);
+        }
     }
+
 
 //    private void setUserData(FirebaseUser user) {
 //        String personName = user.getDisplayName();
@@ -136,6 +180,7 @@ public class Dashboard extends AppCompatActivity {
 //            mAuth.removeAuthStateListener(firebaseAuthListener);
 //        }
 //    }
+
 
 }
 
